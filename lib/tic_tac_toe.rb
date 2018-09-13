ff5fd79cd06af9b0f43eea12cbee04a4a4c6deac
yoan ante <yoanante@gmail.com>
2018-09-13 17:06:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,16 +34,16 @@ def turn(board)
   elsif over?(board)  
     puts "Congratulations #{winner?(board)}!"
   else
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    token = current_player(board)
-    player_move(board, index, token)
-    display_board(board)
-  else
-    turn(board)
-  end
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      token = current_player(board)
+      player_move(board, index, token)
+      display_board(board)
+    else
+      turn(board)
+    end
   end
 end
 
